d3d3e5a8a2596d1bc4662410b6f73e156bf45a22
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-30 03:58:03 +0300

    test_amenity file modified 1

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Contains the class TestConsoleDocs for
-testing the documentation of the console.
+Contains the class TestConsoleDocs
 """
 
 import console
@@ -12,7 +11,7 @@ HBNBCommand = console.HBNBCommand
 
 
 class TestConsoleDocs(unittest.TestCase):
-    """Class for testing the documentation of the console"""
+    """Class for testing documentation of the console"""
 
     def test_pep8_conformance_console(self):
         """Test that console.py conforms to PEP8."""
@@ -29,14 +28,14 @@ class TestConsoleDocs(unittest.TestCase):
                          "Found code style errors (and warnings).")
 
     def test_console_module_docstring(self):
-        """Test for the console.py module docstring."""
+        """Test for the console.py module docstring"""
         self.assertIsNot(console.__doc__, None,
                          "console.py needs a docstring")
         self.assertTrue(len(console.__doc__) >= 1,
                         "console.py needs a docstring")
 
     def test_HBNBCommand_class_docstring(self):
-        """Test for the HBNBCommand class docstring."""
+        """Test for the HBNBCommand class docstring"""
         self.assertIsNot(HBNBCommand.__doc__, None,
                          "HBNBCommand class needs a docstring")
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Contains the TestAmenityDocs class for
-testing the documentation of the Amenity class.
+Contains the TestAmenityDocs classes
 """
 
 # from datetime import datetime
